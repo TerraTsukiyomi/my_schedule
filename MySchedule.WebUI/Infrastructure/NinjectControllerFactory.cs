28c17563175393bda84d975307b967fa004b23ec
TerraTsukiyomi <terratsukiyomi@gmail.com>
2018-03-21 20:47:42 +0200

    br_che_001

---
+++ MySchedule.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySchedule.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : Default
+    {
+    }
+}
